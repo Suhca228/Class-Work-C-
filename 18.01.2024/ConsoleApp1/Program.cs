1b479ef93387529d523cdb60ece7f991894aa6ce
Suhca228 <vovak9855@gmail.com>
2024-01-24 13:26:36 +0200

    24.01.2024

---
+++ 18.01.2024/ConsoleApp1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+
+        }
+}
+}
+
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+
+//Напишіть програму. Користувач вводить число від 
+//100 до 999. Программа виводить кількість сотень, десятків та одиниць.
+
+//int num = 0;
+//int sotni = 0;
+//int desyatki = 0;
+//int odeniczi = 0;
+
+
+
+//Console.WriteLine("Enter a number from 100 to 999:");
+//num = Convert.ToInt16(Console.ReadLine());
+//23
+
+//sotni = num / 100;
+//desyatki = (num - sotni * 100) / 10;
+//odeniczi = num - sotni * 100 - desyatki * 10;
+
+//Console.WriteLine("sotni: " + sotni);
+//Console.WriteLine("desyatki: " + desyatki);
+//Console.WriteLine("odeniczi: " + odeniczi);
+
+
+
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+
+//Random rnd = new Random();
+//int randomNumber = (rnd.Next(1, 100));
+
+//int usernum;
+
+//do
+//{
+//    usernum = Convert.ToInt16(Console.ReadLine());
+//    //Console.WriteLine("Your number: " + usernum);
+
+//    if (usernum < randomNumber)
+//    {
+//        Console.WriteLine(">");
+//    }
+//    else if (usernum > randomNumber)
+//    {
+//        Console.WriteLine("<");
+//    }
+//} while (usernum != randomNumber);
+
+//Console.WriteLine("PERFECT");
