56e00a8b5db112669350853c64545d49026d1197
Suhca228 <vovak9855@gmail.com>
2024-01-25 20:53:16 +0200

    25.01.2024

---
+++ 25.01.2024/25.01.2024/Program.cs
@@ -0,0 +1,72 @@
+﻿using _25._01._2024;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _25._01._2024
+{
+
+    struct Student
+    {
+        public string name;
+        public string surname;
+        public int old;
+
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//Створіть клас студент. Клас має поля: ім'я,
+//прізвище, вік. Створіть масив студентів. Та створіть фун-ю
+//яка буде виводити тільки тих студентів яким більше 18ти років.
+
+
+//Student[] arr = new Student[10];
+
+//arr[0] = new Student { name = "Ivan", surname = "Ivanov", old = 20 };
+//arr[1] = new Student { name = "Olga", surname = "Olgova", old = 19 };
+//arr[2] = new Student { name = "Petro", surname = "Petrov", old = 21 };
+//arr[3] = new Student { name = "Mariya", surname = "Mariyanova", old = 22 };
+//arr[4] = new Student { name = "Sergey", surname = "Sergeyev", old = 23 };
+//arr[5] = new Student { name = "Vasily", surname = "Vasilyev", old = 24 };
+//arr[6] = new Student { name = "Olena", surname = "Olenina", old = 25 };
+//arr[7] = new Student { name = "Dmitriy", surname = "Dmitriyev", old = 26 };
+//arr[8] = new Student { name = "Anna", surname = "Annyeva", old = 27 };
+//arr[9] = new Student { name = "Mykola", surname = "Mykolayev", old = 28 };
+
+
+
+//for (int i = 0; i < 10; i++)
+//    if (arr[i].old >= 18)
+//    {
+//        Console.WriteLine(arr[i].name + " " + arr[i].surname + " " + arr[i].old);
+//    }
+
+
+
