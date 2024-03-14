ba74fddffeb422894643b4c021f54a4993c2dc7a
Suhca228 <vovak9855@gmail.com>
2024-03-14 20:51:22 +0200

    14.03.2024

---
+++ 12.03.2024/12.03.2024/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12._03._2024
+{
+    public class Album
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public int Year { get; set; }
+
+        public Album(string title, string artist, int year)
+        {
+            Title = title;
+            Artist = artist;
+            Year = year;
+        }
+
+        public override string ToString()
+        {
+            return $"Title: {Title}, Artist: {Artist}, Year: {Year}";
+        }
+    }
+
+    public class CollectionManager<T>
+    {
+        private List<T> _items = new List<T>();
+
+        public void AddItem(T item)
+        {
+            _items.Add(item);
+        }
+
+        public void RemoveItem(T item)
+        {
+            _items.Remove(item);
+        }
+
+        public void DisplayItems()
+        {
+            foreach (var item in _items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Choose collection:");
+                Console.WriteLine("1. Numbers");
+                Console.WriteLine("2. Strings");
+                Console.WriteLine("3. Music albums");
+
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        CollectionManager<int> intCollection = new CollectionManager<int>();
+                        intCollection.AddItem(1);
+                        intCollection.AddItem(2);
+                        intCollection.AddItem(3);
+                        intCollection.DisplayItems();
+                        break;
+                    case 2:
+                        CollectionManager<string> stringCollection = new CollectionManager<string>();
+                        stringCollection.AddItem("qwe");
+                        stringCollection.AddItem("rty");
+                        stringCollection.AddItem("uio");
+                        stringCollection.DisplayItems();
+                        break;
+                    case 3:
+                        CollectionManager<Album> albumCollection = new CollectionManager<Album>();
+                        albumCollection.AddItem(new Album("Svit takij", "FLiT", 2003));
+                        albumCollection.AddItem(new Album("Zalupa Rock", "Zlyj Reper Zenyk", 2012));
+                        albumCollection.AddItem(new Album("System of a down", "System of a down", 1998));
+                        albumCollection.DisplayItems();
+                        break;
+                }
+
+                Console.WriteLine("=========================================================");
+            }
+        }
+    }
+}
---
+++ 14.03.2024/14.03.2024/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14._03._2024
+{
+    class Chair
+    {
+        public int Number { get; set; }
+        public string Color { get; set; }
+        public string OccupantName { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Генерація списку стільців
+            List<Chair> chairs = GenerateChairs();
+
+            // Сортування стільців за порядковим номером
+            chairs.Sort((x, y) => x.Number.CompareTo(y.Number));
+
+            // Сортування за кольором, виключаючи червоні біля зелених
+            chairs.Sort((x, y) =>
+            {
+                if (x.Color == "Red" && y.Color == "Green")
+                    return 1;
+                else if (x.Color == "Green" && y.Color == "Red")
+                    return -1;
+                else
+                    return 0;
+            });
+
+            // Сортування за іменами, виключаючи Максима біля Івана
+            chairs.Sort((x, y) =>
+            {
+                if (x.OccupantName == "Max" && y.OccupantName == "Ivan")
+                    return 1;
+                else if (x.OccupantName == "Ivan" && y.OccupantName == "Max")
+                    return -1;
+                else
+                    return 0;
+            });
+
+            // Виведення відсортованих стільців
+            Console.WriteLine("Sorted chairs:");
+            foreach (var chair in chairs)
+            {
+                Console.WriteLine("Номер:" + chair.Number + ", Колір:" + chair.Color + ", Зайнятий:" + chair.OccupantName);
+            }
+
+            Console.ReadLine();
+        }
+
+        static List<Chair> GenerateChairs()
+        {
+            List<Chair> chairs = new List<Chair>();
+            Random random = new Random();
+
+            // Генерація 10 стільців з випадковими параметрами
+            for (int i = 0; i < 10; i++)
+            {
+                Chair chair = new Chair();
+                chair.Number = i + 1;
+                chair.Color = GetRandomColor(random);
+                chair.OccupantName = GetRandomName(random);
+                chairs.Add(chair);
+            }
+
+            return chairs;
+        }
+
+        static string GetRandomColor(Random random)
+        {
+            string[] colors = { "Red", "Green", "Blue" };
+            return colors[random.Next(colors.Length)];
+        }
+
+        static string GetRandomName(Random random)
+        {
+            string[] names = { "Max", "Ivan", "Sergiy" };
+            return names[random.Next(names.Length)];
+        }
+    }
+}
